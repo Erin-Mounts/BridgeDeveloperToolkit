92f69bbf991e3f676045a9a645e0c9b58c095f95
Erin Mounts <erinys62@gmail.com>
2023-01-03 14:44:17 -0800

    Initial Commit

---
+++ BridgeDeveloperToolkit/BridgeDeveloperToolkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BridgeDeveloperToolkitApp.swift
+//  BridgeDeveloperToolkit
+//
+//  Created by Erin Mounts on 1/3/23.
+//
+
+import SwiftUI
+
+@main
+struct BridgeDeveloperToolkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
