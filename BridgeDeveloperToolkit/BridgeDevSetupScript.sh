fb22502d955975bb22e350d7cd34d279251dcb49
Erin Mounts <erinmounts@Wormwood-3.lan>
2023-02-16 09:14:31 -0800

    wip
    pushing current state to GitHub to transfer work to another machine

--- BridgeDeveloperToolkit/BridgeDevSetupScript.sh
+++ BridgeDeveloperToolkit/BridgeDevSetupScript.sh
@@ -23,44 +23,48 @@ fi
 ##################################################################################################
 
 install_ios_dev_tools() {
-# If we're not in an interactive shell, we can't ask questions or wait for input, so we
-# kind of have to assume this has all been taken care of manually beforehand. Most likely,
-# it means we're running as a Run Script Build Phase in Xcode anyway.
+    # If we're not in an interactive shell, we can't ask questions or wait for input, so we
+    # kind of have to assume this has all been taken care of manually beforehand. Most likely,
+    # it means we're running as a Run Script Build Phase in Xcode anyway.
 
-if [[ -t 0 ]]; then
-    print '==========================================='
-    print '= Installing/updating iOS dev environment ='
-    print '==========================================='
-    print "\n"
+    if [[ -t 0 ]]; then
+        print '==========================================='
+        print '= Installing/updating iOS dev environment ='
+        print '==========================================='
+        print "\n"
 
-    xcodeurl='macappstore://apps.apple.com/us/app/xcode/id497799835?mt=12'
-    xcodeInstall=`mdfind -name 'Xcode.app' 2>/dev/null`
-    xcodeUpdated=false
-    if [[ $xcodeInstall  =~ Xcode[.]app$ ]]; then
-        read -q 'updateXcode?Do you want to update or check for updates to Xcode? (Y/N): '
-        if [[ $updateXcode == y ]]; then
+        xcodeurl='macappstore://apps.apple.com/us/app/xcode/id497799835?mt=12'
+        xcodeInstall=`mdfind -name 'Xcode.app' 2>/dev/null`
+        xcodeUpdated=false
+        if [[ $xcodeInstall  =~ Xcode[.]app$ ]]; then
+            read -q 'updateXcode?Do you want to update or check for updates to Xcode? (Y/N): '
+            if [[ $updateXcode == y ]]; then
+                open $xcodeurl
+                print "\n"
+                read -q 'anyKey?When you are done checking and/or updating Xcode, hit any key to continue: '
+                xcodeUpdated=true
+            fi
+        else
             open $xcodeurl
-            print "\n"
-            read -q 'anyKey?When you are done checking and/or updating Xcode, hit any key to continue: '
+            read -q 'anyKey?Please install Xcode from the App Store. When installation has finished, hit any key to continue: '
             xcodeUpdated=true
         fi
-    else
-        open $xcodeurl
-        read -q 'anyKey?Please install Xcode from the App Store. When installation has finished, hit any key to continue: '
-        xcodeUpdated=true
-    fi
 
-    if $xcodeUpdated; then
-        # launch Xcode to trigger request to install/update additional tools
-        xcodeInstall=`mdfind -name 'Xcode.app' 2>/dev/null`; open $xcodeInstall
-        print "\n"
-        read -q 'anyKey?Please install Xcode additional tools. When installation has finished, hit any key to continue: '
+        if $xcodeUpdated; then
+            # launch Xcode to trigger request to install/update additional tools
+            xcodeInstall=`mdfind -name 'Xcode.app' 2>/dev/null`; open $xcodeInstall
+            print "\n"
+            read -q 'anyKey?Please install Xcode additional tools. When installation has finished, hit any key to continue: '
+        fi
+    else
+        print 'If you need to install or update the iOS development environment, please open\
+        a new Terminal window, copy and paste in the following line, then hit return or enter:)'
+        print "cd `pwd`; zsh $0"
     fi
-else
-    print 'If you need to install or update the iOS development environment, please open\
-    a new Terminal window, copy and paste in the following line, then hit return or enter:)'
-    print "cd `pwd`; zsh $0"
-fi
+}
+
+uninstall_ios_dev_tools() {
+    # TODO emm 2023-02-01
 }
 
 ##################################################################################################
@@ -70,70 +74,61 @@ fi
 ##################################################################################################
 
 install_bridge_dev_tools() {
-print "\n"
-print '====================================================='
-print '= Installing/updating Bridge Server dev environment ='
-print '====================================================='
-print "\n"
+    print "\n"
+    print '====================================================='
+    print '= Installing/updating Bridge Server dev environment ='
+    print '====================================================='
+    print "\n"
 
-machine=`uname -m`
-print "Machine type: $machine"
-if [[ $machine =~ ^x86_64 ]]; then
-    print "Detected Intel Mac"
-    corretto8url='https://corretto.aws/downloads/latest/amazon-corretto-8-x64-macos-jdk.pkg'
-    jetbrainsurl='https://download.jetbrains.com/product?code=IIC&latest&distribution=mac'
-else
-    if [[ $machine =~ ^arm ]]; then
-        print "Detected Apple Silicon Mac"
-        corretto8url='https://corretto.aws/downloads/latest/amazon-corretto-8-aarch64-macos-jdk.pkg'
-        jetbrainsurl='https://download.jetbrains.com/product?code=IIC&latest&distribution=macM1'
+    machine=`uname -m`
+    print "Machine type: $machine"
+    if [[ $machine =~ ^x86_64 ]]; then
+        print "Detected Intel Mac"
+        corretto8url='https://corretto.aws/downloads/latest/amazon-corretto-8-x64-macos-jdk.pkg'
+        jetbrainsurl='https://download.jetbrains.com/product?code=IIC&latest&distribution=mac'
     else
-        print "Unsupported machine type $machine"
-        exit 1
+        if [[ $machine =~ ^arm ]]; then
+            print "Detected Apple Silicon Mac"
+            corretto8url='https://corretto.aws/downloads/latest/amazon-corretto-8-aarch64-macos-jdk.pkg'
+            jetbrainsurl='https://download.jetbrains.com/product?code=IIC&latest&distribution=macM1'
+        else
+            print "Unsupported machine type $machine"
+            exit 1
+        fi
     fi
-fi
 
-install_corretto
-#install_intellij
+    install_corretto
+    install_intellij
+}
+
+uninstall_bridge_dev_tools() {
+    uninstall_corretto
+    uninstall_intellij
 }
 
 # install/update Corretto 8
 install_corretto() {
-print "\n"
-print "Downloading latest Corretto 8 .pkg..."
-corretto8pkg="corretto.pkg"
-curl -L $curl_progress "$corretto8url" -o "$corretto8pkg"
+    print "\n"
+    print "Downloading latest Corretto 8 .pkg..."
+    corretto8pkg="corretto.pkg"
+    curl -L $curl_progress "$corretto8url" -o "$corretto8pkg"
 
-open "$corretto8pkg"
-read -q 'anyKey?Please follow the installer prompts to install Corretto 8. When installation has finished, hit any key to continue: '
+    open "$corretto8pkg"
+    read -q 'anyKey?Please follow the installer prompts to install Corretto 8. When installation has finished, hit any key to continue: '
 
-print "Deleting .pkg..."
-rm -f "$corretto8pkg"
+    print "Deleting .pkg..."
+    rm -f "$corretto8pkg"
 
-# check if corretto is being overridden by a "newer" Java version.
-# (Oracle uses a different build number scheme for its Internet applet plug-in which makes it look newer
-# than even later builds of the same minor revision.)
-jhome=`/usr/libexec/java_home`
-internet_plug_in_dir="/Library/Internet Plug-Ins"
-internet_plug_in="$internet_plug_in_dir/JavaAppletPlugin.plugin"
-internet_plug_in_home="$internet_plug_in/Contents/Home"
-hidden_internet_plugin_dir="$internet_plug_in_dir/hidden"
-deployment_properties="deployment.properties"
-while [[ ! ("$jhome" =~ corretto) ]]; do
-    if [[ "$jhome" == "$internet_plug_in_home" ]]; then
-        # hide the plug-in to disable it
-        print "Disabling JavaAppletPlugin..."
-        mkdir -p "$hidden_internet_plugin_dir"
-        mv "$internet_plug_in" "$hidden_internet_plugin_dir/"
-        
-        # update jhome for the next go-round
-        jhome=`/usr/libexec/java_home`
-    else
+    # check if corretto is being overridden by a "newer" Java version.
+    # (Oracle uses a different build number scheme for its Internet applet plug-in which makes it look newer
+    # than even later builds of the same minor revision.)
+    jhome=`/usr/libexec/java_home`
+    if [[ ! ("$jhome" =~ corretto) ]]; then
         # set $JAVA_HOME to corretto in ~/.zshenv
         jhomercfile="~/.zshenv"
         print "A higher-numbered Java version is already installed. Setting JAVA_HOME in \"$jhomercfile\"..."
-        corretto_home=`/usr/libexec/java_home -V 2>&1 | egrep -o '/.*corretto.*$'`
-        corretto_export="export JAVA_HOME=\"$corretto_home\""
+        corretto_home="/usr/libexec/java_home -V 2>&1 | egrep -o '/.*corretto.*$'`
+        java_home_export='export JAVA_HOME=$()
         if [[ ! -e "$jhomercfile" ]]; then
             # create the rc file and add the export
             print "\n# set by $0\n$corretto_export" > "$jhomercfile"
@@ -144,41 +139,45 @@ while [[ ! ("$jhome" =~ corretto) ]]; do
         
         # now load it into the current environment
         source "$jhomercfile"
-
-        # we've forced it to be what we want, so we can break out of the loop now.
-        break
     fi
-done
-print "Corretto 8 is now the active Java version."
+    print "Corretto 8 is now the active Java version."
+}
+
+uninstall_corretto() {
+    # TODO emm 2023-02-01
 }
 
 # install/update IntelliJ
 
 install_intellij() {
-print "\n"
-print "Downloading latest IntelliJ Community Edition .dmg..."
-jetbrainsdmg="jetbrains.dmg"
-curl -L $curl_progress "$jetbrainsurl" -o "$jetbrainsdmg"
+    print "\n"
+    print "Downloading latest IntelliJ Community Edition .dmg..."
+    jetbrainsdmg="jetbrains.dmg"
+    curl -L $curl_progress "$jetbrainsurl" -o "$jetbrainsdmg"
 
-# based loosely on https://stackoverflow.com/a/55869632
-print "Mounting .dmg..."
-volume=$(hdiutil attach -nobrowse "$jetbrainsdmg" | tail -n1 | cut -f3-; exit ${PIPESTATUS[0]})
-print "Copying IntelliJ app to /Applications folder..."
-rsync -a "$volume"/*.app "/Applications/"; synced=$?
-if [[ $synced -eq 0 ]]; then
-    print "IntelliJ app successfully installed in /Applications folder"
-else
-    print "Failed to install IntelliJ app in /Applications folder, rsync exit code $synced"
-fi
-print "Unmounting .dmg..."
-hdiutil detach -force -quiet "$volume"; detached=$?
-if [[ $detached -eq 0 ]]; then
-    print "Successfully unmounted IntelliJ install disk image"
-else
-    print "Failed to unmount IntelliJ install disk image--please do it manually in Disk Utility"
-fi
-print "Deleting .dmg..."
-rm -f "$jetbrainsdmg"
+    # based loosely on https://stackoverflow.com/a/55869632
+    print "Mounting .dmg..."
+    volume=$(hdiutil attach -nobrowse "$jetbrainsdmg" | tail -n1 | cut -f3-; exit ${PIPESTATUS[0]})
+    print "Copying IntelliJ app to /Applications folder..."
+    rsync -a "$volume"/*.app "/Applications/"; synced=$?
+    if [[ $synced -eq 0 ]]; then
+        print "IntelliJ app successfully installed in /Applications folder"
+    else
+        print "Failed to install IntelliJ app in /Applications folder, rsync exit code $synced"
+    fi
+    print "Unmounting .dmg..."
+    hdiutil detach -force -quiet "$volume"; detached=$?
+    if [[ $detached -eq 0 ]]; then
+        print "Successfully unmounted IntelliJ install disk image"
+    else
+        print "Failed to unmount IntelliJ install disk image--please do it manually in Disk Utility"
+    fi
+    print "Deleting .dmg..."
+    rm -f "$jetbrainsdmg"
+}
+
+uninstall_intellij() {
+    # TODO emm 2023-02-01
 }
 
 # install/update MacPorts
@@ -214,11 +213,15 @@ rm -f "$jetbrainsdmg"
 ##################################################################################################
 
 install_android_dev_tools() {
-print "\n"
-print '==============================================='
-print '= Installing/updating Android dev environment ='
-print '==============================================='
-print "\n"
+    print "\n"
+    print '==============================================='
+    print '= Installing/updating Android dev environment ='
+    print '==============================================='
+    print "\n"
+}
+
+uninstall_android_dev_tools() {
+    # TODO emm 2023-02-01
 }
 
 ##################################################################################################
@@ -228,11 +231,15 @@ print "\n"
 ##################################################################################################
 
 install_web_dev_tools() {
-print "\n"
-print '==========================================='
-print '= Installing/updating Web dev environment ='
-print '==========================================='
-print "\n"
+    print "\n"
+    print '==========================================='
+    print '= Installing/updating Web dev environment ='
+    print '==========================================='
+    print "\n"
+}
+
+uninstall_web_dev_tools() {
+    # TODO emm 2023-02-01
 }
 
 #install_ios_dev_tools
